1b877011a66d0c676b002186b63a296f55967f3e
franz-xaver-hallhuber <blumenimhaar@freudenkinder.de>
2019-01-12 00:11:37 +0100

    new logging

--- gbot.py
+++ gbot.py
@@ -19,22 +19,6 @@ import socket
 from objects import *
 
 # logging
-
-
-class StreamToLogger(object):
-   """
-   Fake file-like stream object that redirects writes to a logger instance.
-   """
-   def __init__(self, logger, log_level=logging.INFO):
-      self.logger = logger
-      self.log_level = log_level
-      self.linebuf = ''
-
-   def write(self, buf):
-      for line in buf.rstrip().splitlines():
-         self.logger.log(self.log_level, line.rstrip())
-
-
 logging.basicConfig(
    level=logging.DEBUG,
    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
@@ -42,13 +26,16 @@ logging.basicConfig(
    filemode='w+'
 )
 
-stdout_logger = logging.getLogger('STDOUT')
-sl = StreamToLogger(stdout_logger, logging.INFO)
-sys.stdout = sl
 
-stderr_logger = logging.getLogger('STDERR')
-sl = StreamToLogger(stderr_logger, logging.ERROR)
-sys.stderr = sl
+logger = logging.getLogger('STDOUT')
+
+stdhandler = logging.StreamHandler(sys.stdout)
+stdhandler.setLevel(logging.INFO)
+logger.addHandler(stdhandler)
+
+errhandler = logging.StreamHandler(sys.stderr)
+errhandler.setLevel(logging.ERROR)
+logger.addHandler(errhandler)
 
 def l2c(msg):
     print("[" + time.strftime("%d/%m %H:%M:%S", time.gmtime()) + "] " + msg)
